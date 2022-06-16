b07880fa65c3543af914740e12dc4a15a3869e5a
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-16 17:32:38 +0200

    Update fornitoreDAO.c

--- fornitoreDAO.c
+++ fornitoreDAO.c
@@ -5,8 +5,7 @@ extern MYSQL *conn;
 
 unsigned int fornitoreDAO(fornitore *f) {
     char query[4096];
-    sprintf(query, "INSERT INTO Fornitori (idFornitore, nome, indirizzo, citta, provincia, email, sitoweb, note, telefono, fax) VALUES(%lu, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");",getId(f), getNome(f), getIndirizzo(f), getCitta(f), getProvincia(f), geteMail(f),getSitoWeb(f), getNote(f), getTelefono(f), getFax(f));
-    printf("%s\n", query);
+    sprintf(query, "INSERT INTO Fornitori (idFornitore, nome, indirizzo, citta, provincia, email, sitoweb, telefono, fax) VALUES(%lu, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");",getId(f), getNome(f), getIndirizzo(f), getCitta(f), getProvincia(f), geteMail(f),getSitoWeb(f), getTelefono(f), getFax(f));
     if (mysql_real_query(conn, query, strlen(query))!=0)
         return mysql_errno(conn);
     return 0;
